7169a2ca2354eaec1d8536edd1bf03efbe73e28e
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2025-02-24 14:23:21 +0000

    feat(Unknown Instruction):

--- include/System/CPU/CPU.hpp
+++ include/System/CPU/CPU.hpp
@@ -22,6 +22,7 @@ class CPU {
     uint16_t PC = 0x0000;
     uint16_t SP = 0x8200;
     uint16_t IR = 0;
+    uint16_t lastInstruction = 0;
 
     Memory *memory;
     Flags flags;
@@ -37,6 +38,7 @@ class CPU {
     SignedOperands getSignedOperandsFromData(uint16_t data);
 
     uint16_t getULADestination(uint16_t data);
+    uint16_t getMemoryAddress(string data);
 
     void setResultInRegister(uint32_t result, uint16_t address);
 
